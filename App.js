b5f73d06a5518055afbc45d93018e3510d7f00c4
yves_guerin <yguerin@oui.sncf>
2020-01-22 13:50:10 +0100

    camera

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import MainView from './Components/MainView'
+import Camera from './Components/Camera'
 
 export default class App extends React.Component {
   render() {
     return (
-      <MainView/>
+      <Camera/>
     );
   }
 }
---
+++ Components/Camera.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { RNCamera } from 'react-native-camera';
+import {View} from 'react-native'
+
+export default class Camera extends React.Component {
+    render() {
+        return (
+            <View style={{flex: 1}}>
+               <RNCamera
+                    ref={ref => {
+                        this.camera = ref;
+                    }}
+                    style={{
+                        flex: 1,
+                        width: '100%',
+                    }}
+                    captureAudio={false}
+                    androidCameraPermissionOptions={{
+                        title: 'Permission to use camera',
+                        message: 'We need your permission to use your camera',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                    androidRecordAudioPermissionOptions={{
+                        title: 'Permission to use audio recording',
+                        message: 'We need your permission to use your audio',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                    onGoogleVisionBarcodesDetected={({ barcodes }) => {
+                        console.log(barcodes);
+                    }}>
+                </RNCamera>
+            </View>
+        );
+      }
+}
